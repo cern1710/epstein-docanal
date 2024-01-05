de429b7ed4ab55260d7be48a1978e49427fcf502
Samuel Wu <cernunnos1710@gmail.com>
2024-01-06 00:46:00 +0800

    defined function to parse and tokenize text from our document using nltk and pypdf

--- src/parser.py
+++ src/parser.py
@@ -1,7 +1,32 @@
-from pypdf import PdfReader
+'''
+Uncomment these lines if you haven't downloaded punkt already!
+'''
+# import nltk
+# nltk.download('punkt')
 
-reader = PdfReader("../doc.pdf")
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text()
-print(text)
+from pypdf import PdfReader
+import re
+from nltk.tokenize import word_tokenize
+
+def parse_doc(pdf_path):
+    reader = PdfReader(pdf_path)
+    all_text = ""
+    for page in reader.pages:
+        all_text += page.extract_text() + '\n'
+    return all_text
+
+def clean_doc(lines):
+    cleaned_text = []
+    for line in lines:
+        line = line.lower()
+        line = re.sub(r"[,.\"\'!@#$%^&*(){}?/;`~:<>+=-\\]", "", line)
+        tokens = word_tokenize(line)
+        cleaned_text += [word for word in tokens if word.isalpha()]
+    return cleaned_text
+
+def extract_text(pdf_path):
+    text = parse_doc(pdf_path)
+    cleaned_text = clean_doc(text)
+    return cleaned_text
+
+extract_text("../doc.pdf")
