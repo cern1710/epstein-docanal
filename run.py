d624a2347340b6959580b57ae7243ad9555e6ab6
Samuel Wu <cernunnos1710@gmail.com>
2024-01-06 14:29:13 +0800

    fast tokenization; save words in txt file instead of reading pdf

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
 from src import markov, parser
+import os
 
-doc_path = "doc.pdf"
+doc_path = "docs/doc.pdf"
+extracted_path = "docs/extracted_text.txt"
+check_path = "docs/check.txt"
 
 if __name__ == "__main__":
-    parser.extract_text(doc_path)
-    model = markov.HiddenMarkovModel
+    if os.path.exists(extracted_path):
+        corpus = parser.read_extracted_doc(extracted_path)
+        print(corpus)
+    else:
+        corpus = parser.extract_text(doc_path)
+        file = open(extracted_path, 'w+')
+        for word in corpus:
+            file.write(word + ' ')
+        file.close()
--- src/parser.py
+++ src/parser.py
@@ -4,19 +4,21 @@ Uncomment these lines if you haven't downloaded punkt already!
 # import nltk
 # nltk.download('punkt')
 
-from pypdf import PdfReader
 import re
+from pypdf import PdfReader
+from pathlib import Path
 from nltk.tokenize import word_tokenize
 
-def parse_doc(pdf_path):
+def parse_doc(pdf_path: str) -> str:
     reader = PdfReader(pdf_path)
     all_text = ""
     for page in reader.pages:
         all_text += page.extract_text() + '\n'
     return all_text
 
-def clean_doc(lines):
+def clean_doc(lines: str) -> list:
     cleaned_text = []
+    lines = lines.split('\n')
     for line in lines:
         line = line.lower()
         line = re.sub(r"[,.\"\'!@#$%^&*(){}?/;`~:<>+=-\\]", "", line)
@@ -24,7 +26,14 @@ def clean_doc(lines):
         cleaned_text += [word for word in tokens if word.isalpha()]
     return cleaned_text
 
-def extract_text(pdf_path):
+def extract_text(pdf_path: str) -> list:
     text = parse_doc(pdf_path)
     cleaned_text = clean_doc(text)
     return cleaned_text
+
+def read_extracted_doc(pdf_path: str) -> list:
+    corpus = []
+    text = Path(pdf_path).read_text()
+    tokens = word_tokenize(text)
+    corpus += [word for word in tokens]
+    return corpus
